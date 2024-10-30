4e9370172c239002b0dcd232bc58630f2c279d7a
farhioi <163438036+farhioi@users.noreply.github.com>
2024-10-30 14:08:12 +0200

    Add files via upload

---
+++ UI/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import NewExpense from '../components/NewExpense/NewExpense';
+import Expenses from '../components/Expenses/Expenses'; 
+
+const dummy_expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date("2022-07-14"),
+    },
+    {id: 'e2', title: 'New TV', amount: 799.49, date: new Date("2023-01-12"),},
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 300.00,
+      date: new Date("2023-01-01"),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date("2022-05-12"),
+    },
+];
+
+const App = () => {
+
+  const [expenses, setExpenses] = useState(dummy_expenses);
+
+  const addExpenseHandler = (expense) => { 
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];  
+    });
+  };
+  
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+      <p>Farhio / VAMK / BIT</p>
+    </div>
+  );
+}
+
+export default App;
